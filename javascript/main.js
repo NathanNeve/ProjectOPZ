a7767916b8cfa20692f5934576d90b2a24dd9f1a
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2023-04-24 15:39:40 +0200

    javascript van alle sliders in main.js gezet

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    speed: 300,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
+});
